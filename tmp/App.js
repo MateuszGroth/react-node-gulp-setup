0f058c5b1c18cd36d94add0694180d2ce72e73f4
Mateusz Groth <MateuszG@advaoptical.com>
2020-02-11 15:32:26 +0100

    gitignore and code

--- tmp/App.js
+++ tmp/App.js
@@ -10,5 +10,5 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function App() {
-  return _react.default.createElement("div", null, "Hello World!");
+  return _react.default.createElement("div", null, "Hello World! Testuje drugie");
 }
